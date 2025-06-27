3e65659049a2cf8aa3e2bb9d63eb03770f11d953
rodrigorleite <rodrigo.junior.queiroz@gmail.com>
2025-06-27 16:40:02 -0300

    jogo da velha

---
+++ Jogo da velha/Js/Script.js
@@ -0,0 +1,19 @@
+var jogador = "X";
+
+function jogar (celula){
+  
+    if(celula.innerHTML == ""){
+
+        celula.innerHTML = jogador;
+    }
+
+    if (jogador == "X"){
+        jogador = "O"
+    }else(
+        jogador ="X"
+    )
+};
+
+function reiniciar (){
+    window.location.reload();
+}
--- Landing Page/Js/Script.js
+++ Landing Page/Js/Script.js
@@ -1,7 +1,16 @@
+<<<<<<< HEAD
 $(document).ready(function () {
     $("#mobile_btn").on("click", function () {
         $('#mobile_menu').toggleClass('active');
         $('#mobile_btn').find("i").toggleClass('fa-x');
 
     });
+=======
+$(document).ready(function () {
+    $("#mobile_btn").on("click", function () {
+        $('#mobile_menu').toggleClass('active');
+        $('#mobile_btn').find("i").toggleClass('fa-x');
+
+    });
+>>>>>>> 83ecd313ad051125f88e4b2910ca9350a03db3cb
 });
