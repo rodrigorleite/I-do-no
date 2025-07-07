3df4196d766b12ed922585b0c5d99bb100177676
rodrigorleite <rodrigo.junior.queiroz@gmail.com>
2025-07-07 16:27:24 -0300

    agora é possivel fechar o tigrinho

--- Python/Tigrinho.py
+++ Python/Tigrinho.py
@@ -42,11 +42,29 @@ def sortear():
          tk.Button(janela, text=str(i), command=lambda i=i: [janela.destroy(), verificarescolha(i)]).pack(pady=10)
 # sortear()
 
+def exibirregras():
+    regras =(
+        "       Rules of Game \n"
+        "\n"
+        "\n"
+        "1 | Choise a nunber in 1 or 6. \n"
+        "\n"
+        "2 | if you coise a number null your computer is turn off! \n"
+        "\n"
+        "3 | Caso contrario o jogo continua. \n"
+        "\n"
+        "                   Good Luck, you need!"
+    )
+    messagebox.showinfo("Regras", regras)
+
+def sair():
+    root.destroy()
+
 root = tk.Tk()
 root.title("Fortune Tigger!")
 tk.Label(root, text="Welcome to Fortune Tigger", font=("Arial", 20)).pack(pady=15)
 tk.Button(root, text="Start Game", width=20, command=sortear).pack(pady=10)
-tk.Button(root, text="View Rules", width=20, command=sortear).pack(pady=10)
-tk.Button(root, text="Exit", width=20, command=sortear).pack(pady=10)
+tk.Button(root, text="View Rules", width=20, command=exibirregras).pack(pady=10)
+tk.Button(root, text="Exit", width=20, command=sair).pack(pady=10)
 
 root.mainloop()
